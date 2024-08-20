38aa1ee6e8bf90730226c0f262d11961aefc8cb1
setu <setu@setu.com>
2024-08-21 03:46:27 +0600

    basic setup

---
+++ frontend/app/dashboard/layout.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Sidebar from "../ui/dashboard/sidebar/sidebar";
+import Navbar from "../ui/dashboard/navbar/navbar";
+
+const Layout = ({ children } : any) => {
+  return (
+    <div>
+      <div>
+        <Sidebar></Sidebar>
+      </div>
+      <div>
+        <Navbar></Navbar>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
