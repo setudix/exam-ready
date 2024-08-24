c403812d9d9fa8305f8be87c50dd5122702440eb
setu <setu@setu.com>
2024-08-24 21:11:48 +0600

    backend template

---
+++ examReady/src/main/java/com/du/yiit/examReady/ExamReadyApplication.java
@@ -0,0 +1,13 @@
+package com.du.yiit.examReady;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamReadyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamReadyApplication.class, args);
+	}
+
+}
---
+++ examReady/src/test/java/com/du/yiit/examReady/ExamReadyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.du.yiit.examReady;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamReadyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
